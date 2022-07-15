e07fd13637f9311ad6b5be7e769f72725e913697
yogesh-110 <95060767+yogesh-110@users.noreply.github.com>
2022-07-15 18:53:53 +0545

    first project

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,93 @@
+﻿using CrudApplication.Data;
+using CrudApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationContext context;
+
+        public StudentController(ApplicationContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = context.Students.ToList();
+            return View(result);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student model)
+        {
+            if (ModelState.IsValid)
+            {
+                var stu = new Student()
+                {
+                    Name = model.Name,
+                    Age = model.Age,
+                    Address = model.Address,
+                    Phone = model.Phone,
+                };
+                context.Students.Add(stu);
+                context.SaveChanges();
+                TempData["error"] = "Record Saved!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Emply Field can't Submit";
+                return View(model);
+            }
+
+        }
+        public IActionResult Delete(int id)
+        {
+            var stu = context.Students.SingleOrDefault(e => e.Id == id);
+            context.Students.Remove(stu);
+            context.SaveChanges();
+            TempData["error"] = "Record Delete";
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var stu= context.Students.SingleOrDefault(e => e.Id == id);
+            var result = new Student()
+            {
+                Name = stu.Name,
+                Age = stu.Age,
+                Address=stu.Address,
+                Phone=stu.Phone
+            };
+            return View(result);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student model)
+        {
+            var stu = new Student()
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Age = model.Age,
+                Address = model.Address,
+                Phone = model.Phone
+            };
+            context.Students.Update(stu);
+            context.SaveChanges();
+            TempData["error"] = "Record Updated";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudApplication.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "* Name can't be blank")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "* Age can't be blank")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "* Address can't be blank")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "* phoneno can't be blank")]
+        public int Phone { get; set; }
+    }
+}
